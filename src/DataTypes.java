d828404f2a639e554546d401e63248aa2e2dd88c
chendi12 <Cc@20020123>
2024-09-16 18:13:58 -0400

    Incorrectly change the main branch, now restoring it

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -7,7 +7,7 @@ public class DataTypes {
     }
     public static long sum(List<Integer> numbers) {
 
-        long s = 0;
+        int s = 0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
